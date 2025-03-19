f134b9442a7426638de1427bfb49f26827eab46f
mamorum <mamorum@users.noreply.github.com>
2025-03-20 02:48:55 +0900

    Update mk_yf_mtd_csv.py

--- yftl/cmd/monthly/mk_yf_mtd_csv.py
+++ yftl/cmd/monthly/mk_yf_mtd_csv.py
@@ -1,9 +1,15 @@
-import yfinance as yf
+# 使用例
+# > python cmd\monthly\mk_yf_mtd_csv.py ^^SPX 2022 2023
+# > python cmd\monthly\mk_yf_mtd_csv.py JPY=X 2023 2023
 
-# 取得する月間収益率
-ticker = '^SPX'
-start = 2022
-end = 2024
+import sys
+import yfinance as yf
+import lib.ticker as lticker
+
+# コマンドライン引数の取得
+ticker = sys.argv[1]
+start = int(sys.argv[2])
+end = int(sys.argv[3])
 
 # 日次データの範囲（少し余分に）
 symd = f'{start-1}-12-20'
@@ -29,6 +35,10 @@ df['MTD'] = df['Close'].pct_change()
 # 行を逆順に変更（降順）
 df = df.iloc[::-1]
 
+# 出力ファイルを決定
+lticker = lticker.lower(ticker)
+csvfile = f'{lticker}-mtd-{start}-{end}.csv'
+
 # CSVに出力
 fmt = '%Y-%m-%d'
-df.to_csv('spx-mtd.csv', date_format=fmt)
+df.to_csv(csvfile, date_format=fmt)
