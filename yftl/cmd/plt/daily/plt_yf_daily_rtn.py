0ddc52b144695281e237e383f70c66f3b982ed21
mamorum <mamorum@users.noreply.github.com>
2025-03-28 09:16:29 +0900

    Update plt_yf_daily_rtn.py

--- yftl/cmd/plt/daily/plt_yf_daily_rtn.py
+++ yftl/cmd/plt/daily/plt_yf_daily_rtn.py
@@ -1,13 +1,13 @@
 # 使用例
-# > python cmd\plt\daily\plt_yf_daily_rtn.py ^^SPX 2022 2023
-# > python cmd\plt\daily\plt_yf_daily_rtn.py ^^SPX 2023 2023
+# > python cmd\plt\daily\plt_yf_daily_rtn.py ^^SPX,^^NDX 2022 2023
+# > python cmd\plt\daily\plt_yf_daily_rtn.py ^^SPX,^^NDX 2023 2023
 
 import sys
 import matplotlib.pyplot as plt
 import lib.yfi as lyf
 
 # コマンドライン引数の取得
-ticker = sys.argv[1]
+tickers = sys.argv[1].split(',')
 start = int(sys.argv[2])
 end = int(sys.argv[3])
 
@@ -15,8 +15,9 @@ end = int(sys.argv[3])
 plt.figure(figsize=(12, 6))
 
 # リターンの推移をプロット
-df = lyf.rtn_df(ticker, start, end)
-plt.plot(df.index, df["RTN"], label=ticker)
+for ticker in tickers:
+  df = lyf.rtn_df(ticker, start, end)
+  plt.plot(df.index, df["RTN"], label=ticker)
 
 # グラフのラベルとタイトル
 plt.xlabel("Date")
